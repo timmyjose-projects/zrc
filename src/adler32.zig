8e876e53bea70c8b39b54a30cc007533c866f40c
Timmy Jose <zoltan.jose@gmail.com>
2020-07-18 21:49:16 +0530

    Working version - all tests passing

--- src/adler32.zig
+++ src/adler32.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 const Checksum = @import("./checksum.zig").Checksum;
 
 /// A checksum implementation - using the Adler-32 algorithm.
@@ -5,28 +8,54 @@ pub fn Adler32() type {
     return struct {
         const Self = @This();
         const ChecksumType = u32;
+        const PRIME_MOD = 65521;
+        const default_adler32_val = 0;
 
-        bytes: []const u8,
+        bytes: ArrayList(u8),
         checksum: Checksum(ChecksumType),
-        checksum_value: ?ChecksumType,
+        checksum_value: ChecksumType,
+        allocator: *Allocator,
 
-        pub fn init() Self {
+        pub fn init(allocator: *Allocator) Self {
             return Self{
-                .bytes = undefined,
+                .bytes = ArrayList(u8).init(allocator),
                 .checksum = Checksum(ChecksumType){
                     .reset_fn = reset,
-                    .update_byte_fn = updateByte,
+                    .update_byte_array_range_fn = updateByteArrayRange,
+                    .close_fn = close,
                 },
-                .checksum_value = null,
+                .checksum_value = default_adler32_val,
+                .allocator = allocator,
             };
         }
 
         fn reset(checksum: *Checksum(ChecksumType)) void {
             const self = @fieldParentPtr(Self, "checksum", checksum);
+            self.checksum_value = default_adler32_val;
+            self.bytes.shrink(0);
         }
 
-        fn updateByte(checksum: *Checksum(ChecksumType), b: u8) void {
+        fn close(checksum: *Checksum(ChecksumType)) void {
             const self = @fieldParentPtr(Self, "checksum", checksum);
+            self.bytes.deinit();
+        }
+
+        /// Simple, unoptimised implementation
+        fn updateByteArrayRange(checksum: *Checksum(ChecksumType), bs: []const u8, offset: usize, len: usize) !void {
+            const self = @fieldParentPtr(Self, "checksum", checksum);
+
+            for (bs[offset .. offset + len]) |b| {
+                try self.bytes.append(b);
+            }
+
+            var r: ChecksumType = 1;
+            var s: ChecksumType = 0;
+            for (self.bytes.items) |b| {
+                r = @mod(r + b, PRIME_MOD);
+                s = @mod(r + s, PRIME_MOD);
+            }
+
+            self.checksum_value = (s << 16) | r;
         }
     };
 }
--- src/crc-32.zig
+++ src/crc-32.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+const Allocator = std.mem.Allocator;
 const Checksum = @import("./checksum.zig").Checksum;
 
 /// A checksum implementation - using the CRC-32 algorithm.
@@ -5,28 +8,56 @@ pub fn Crc32() type {
     return struct {
         const Self = @This();
         const ChecksumType = u32;
+        const default_crc32_val = 0xffffffff;
 
-        bytes: []u8,
+        bytes: ArrayList(u8),
         checksum: Checksum(ChecksumType),
-        checksum_value: ?ChecksumType,
+        checksum_value: ChecksumType,
+        allocator: *Allocator,
 
-        pub fn init() Self {
+        pub fn init(allocator: *Allocator) Self {
             return Self{
-                .bytes = undefined,
+                .bytes = ArrayList(u8).init(allocator),
                 .checksum = Checksum(ChecksumType){
                     .reset_fn = reset,
-                    .update_byte_fn = updateByte,
+                    .update_byte_array_range_fn = updateByteArrayRange,
+                    .close_fn = close,
                 },
-                .checksum_value = null,
+                .checksum_value = default_crc32_val,
+                .allocator = allocator,
             };
         }
 
         fn reset(checksum: *Checksum(ChecksumType)) void {
             const self = @fieldParentPtr(Self, "checksum", checksum);
+            self.checksum_value = default_crc32_val;
+            self.bytes.shrink(0);
         }
 
-        fn updateByte(checksum: *Checksum(ChecksumType), b: u8) void {
+        fn close(checksum: *Checksum(ChecksumType)) void {
             const self = @fieldParentPtr(Self, "checksum", checksum);
+            self.bytes.deinit();
+        }
+
+        /// Source: Hacker's Delight - Chapter 14 (CRC)
+        fn updateByteArrayRange(checksum: *Checksum(ChecksumType), bs: []const u8, offset: usize, len: usize) !void {
+            const self = @fieldParentPtr(Self, "checksum", checksum);
+
+            for (bs[offset .. offset + len]) |b| {
+                try self.bytes.append(b);
+            }
+
+            var crc: ChecksumType = default_crc32_val;
+            for (self.bytes.items) |b| {
+                crc = crc ^ @intCast(ChecksumType, b);
+                var ctr: i32 = 7;
+                while (ctr >= 0) : (ctr -= 1) {
+                    var mask = -(@intCast(i64, crc) & 1);
+                    crc = (crc >> 1) ^ @intCast(ChecksumType, 0xedb88320 & mask);
+                }
+            }
+
+            self.checksum_value = ~crc;
         }
     };
 }
